15a1ecac10ebf06e93c227e43ba387626cc14c5d
emelyarellano1 <emelyac91@hotmail.com>
2016-10-30 22:58:06 -0300

    MODIFICACIONES

--- js/main.js
+++ js/main.js
@@ -9,111 +9,113 @@ El campo password no puede ser igual a "password" ó "123456" ó "098754"
 El valor seleccionado de bicis, debe ser una de las opciones presentadas */
 
 
-function validacionForm(){
+function validateForm(){
 		var letras = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
 
 		function name(){
-		 	var name = document.getElemetById('name').value;
+		 	var name = document.getElementById('name').value;
 		 	if (name == null || name.length == 0 || /^\s+$/.test(name)){
 		 		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Ingresar tu nombre');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Ingresar tu nombre, Ej: Emelys');
+		 		spn.box.appendChild(cnt);
+		 		spn.box.appendChild(spn);
 		 		return false;
 		 	} else if (!letras.test(name)){
 		 		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Solo letras');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Solo letras');
+		 		spn.appendChild(cnt);
+		 		dox.appendChild(spn);
 		 		return true;
 		 	} else if(name.charAt(0) != name.charAt(0).toUpperCase()){
 		 		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Comezar con masyuscola');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Comezar con masyuscola');
+		 		spn.appendChild(cnt);
+		 		dox.appendChild(spn);
 		 	}
 		}
-		 name();
+		name();
 
 		function lastname(){
-		 	var lastname = document.getElemetById('lastname').value;
+		 	var lastname = document.getElementById('lastname').value;
 		 	if(lastname == null || lastname.length == 0 || lastname.length == 0 || /^\s+$/.test(lastname)){
-		 		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Ingresar tu apellido');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+		 		var box = document.getElementsByClassName('input-4')[1];
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Ingresar tu apellido');
+		 		spn.appendChild(cnt);
+		 		dox.appendChild(spn);
 		 		return true;
 		 	} else if(!letras.test(lastname)){
-		 		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Solo letras');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+		 		var box = document.getElementsByClassName('input-box')[1];
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Solo letras');
+		 		spn.appendChild(cnt);
+		 		dox.appendChild(spn);
 		 		return true;
 		 	} else if(lastname.charAt(0) != lastname.charAt(0).toUpperCase()) {
-		 		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('La primera letra debe ser Mayuscula, Ej: Arellano');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+		 		var box = document.getElementsByClassName('input-box')[1];
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('La primera letra debe ser Mayuscula, Ej: Arellano');
+		 		spn.appendChild(cnt);
+		 		dox.appendChild(spn);
 		 	}
 		 } 
 		lastname();
 
 		function correoElectronico(){
 			var simbolos = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
-			var correoElectronico = document.getElemetById('input-email').value;
+			var correoElectronico = document.getElementById('input-email').value;
 			if (correoElectronico === null || correoElectronico.length === 0){
-				var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Ingresa tu correo electronico');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+				var box = document.getElementsByClassName('input-box')[2];
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Ingresa tu correo electronico');
+		 		spn.appendChild(cnt);
+		 		dox.appendChild(spn);
 				return false;
 			} else if(simbolos.test(correoElectronico)){
-				var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Verifica tu correo electronico');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+				var box = document.getElementsByClassName('input-box')[2];
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Verifica tu correo electronico');
+		 		spn.appendChild(cnt);
+		 		dox.appendChild(spn);
 				return true;
 			}
 	    }
 	    correoElectronico();
 
 	    function contrasenia(){
-	    	var contrasenia = document.getElemetById('input-Password').value;
-	    	if (contrasenia == null || contrasenia.length == 0) {
-	    		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Ingresa tu contraseñia');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
+	    	var contrasenia = document.getElementById('input-Password').value;
+	    	if (contrasenia == null || contrasenia.length == 0){
+	    		var box = document.getElementsByClassName('input-box')[3];
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Ingresa tu contraseña');
+		 		spn.appendChild(cnt);
+		 		box.appendChild(spn);
 	    		return false;
 	    	} else if(contrasenia.length < 6){
-	    		var box = document.getElementsByClassName('input-box')[0];
-		 		var span = document.getElemet('span');
-		 		var disn = document.createTextNode('Tu contraseña debe tener mas de 6 caracteres');
-		 		span.appendChild(disn);
-		 		dox.appendChild(span);
-	    		return false;
+	    		var box = document.getElementsByClassName('input-box')[3];
+		 		var spn = document.createElement('span');
+		 		var cnt = document.createTextNode('Tu contraseña debe tener mas de 6 caracteres');
+		 		spn.appendChild(cnt);
+		 		box.appendChild(spn);
+	    		return true;
 	    	}
 	    }
 	    contrasenia();
 
 	    function modeloBici(){
-	    	var selector = document.getElemetsByTagNeme('select');
-	    	var modelo = select.value;
-	    	var box = document.getElementsByClassName('input-box')[0];
-		 	var span = document.getElemet('span');
-		 	var disn = document.createTextNode('Selecciona una opción');
-		 	span.appendChild(disn);
-		 	dox.appendChild(span);
+	    	var selector = document.getElementsByTagName('select').value;
+	    	var modelo = selector.value;
+	    	var box = document.getElementsByClassName('input-box')[4];
+		 	var spn = document.createElement('span');
+		 	var cnt = document.createTextNode('Selecciona una opción');
+		 	spn.appendChild(cnt);
+		 	box.appendChild(spn);
 	    	return modelo == 'Urbana' || modelo == 'Treking' || modelo == 'Eléctrica' || modelo == 'Estática';
 	    }
 	    modeloBici();
 	}
+
+validateForm();
